0eaf28f8a12a95e62991cc142df6547b8107788b
Mark-RSK <mark.pitt@rocksolidknowledge.com>
2017-04-07 10:57:58 +0100

    Initial checkin of failing batch remove from UITableView

---
+++ UITableViewTest.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+
+namespace UITableViewTest.Core.ViewModels
+{
+	public class FirstViewModel
+		: MvxViewModel
+	{
+		public MvxObservableCollection<string> Items { get; }
+		public EventHandler<bool> ModifyList = delegate { };
+
+		public FirstViewModel()
+		{
+			Items = new MvxObservableCollection<string>();
+
+		}
+
+		public override void Start()
+		{
+			base.Start();
+			Add(this);
+		}
+
+		private async void Add(FirstViewModel instance)
+		{
+			while (true)
+			{
+				ModifyList(this, true);
+				for (int i = 0; i < 100; i++)
+				{
+					Items.Add($"Item {i}");
+				}
+				ModifyList(this, false);
+
+				await Task.Delay(10000);
+
+				ModifyList(this, true);
+				for (int i = 0; i < 100; i++)
+				{
+					Items.Remove($"Item {i}");
+				}
+				ModifyList(this, false);
+
+				await Task.Delay(10000);
+			}
+		}
+	}
+}
---
+++ UITableViewTest.iOS/Views/FirstView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using MvvmCross.iOS.Views;
+using UIKit;
+using UITableViewTest.Core.ViewModels;
+
+namespace UITableViewTest.iOS.Views
+{
+	[MvxFromStoryboard]
+	public partial class FirstView : MvxViewController<FirstViewModel>
+	{
+		public FirstView(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			ViewModel.ModifyList += (sender, modifyList) =>
+			{
+				if (modifyList)
+					table.BeginUpdates();
+				else
+					table.EndUpdates();
+			};
+			var tableSource = new MyTableViewSource(table);
+			table.Source = tableSource; 
+
+			var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+			set.Bind(tableSource).To(vm => vm.Items);
+			set.Apply();
+		}
+
+	}
+
+	public class MyTableViewSource : MvxTableViewSource
+	{
+		public MyTableViewSource(UITableView tableView)
+			: base(tableView)
+		{
+			tableView.RegisterClassForCellReuse(typeof(UITableViewCell), "MyTableViewSource");
+			UseAnimations = true;
+			AddAnimation = UITableViewRowAnimation.Top;
+			RemoveAnimation = UITableViewRowAnimation.Top;
+		}
+
+		protected override UITableViewCell GetOrCreateCellFor(UITableView tableView, NSIndexPath indexPath, object item)
+		{
+			var cell = tableView.DequeueReusableCell("MyTableViewSource", indexPath);
+
+			cell.TextLabel.Text = item as string;
+
+			return cell;
+		}
+	}
+}
